92909cad33891de06cec66ca2ce420b0aa0fd986
haipeng li <haipeng.li@net263.com>
2019-04-25 17:19:12 +0800

    sip日志打印

--- app/src/main/java/com/example/linphonetest/MainActivity.java
+++ app/src/main/java/com/example/linphonetest/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.linphonetest;
 
 import android.Manifest;
-import android.content.Context;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,19 +9,15 @@ import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
-import com.example.linphone.LinphoneManager;
-import com.example.linphone.SipUtils;
+import com.ajsip.LinphoneManager;
+import com.ajsip.ajsipmanager.AJSipManager;
+import com.ajsip.callback.AJCoreListenerStub;
+import com.ajsip.state.AJCallState;
+import com.ajsip.state.AJGlobalState;
+import com.ajsip.state.AJRegistrationState;
 import com.example.linphonetest.databinding.ActivityMainBinding;
 
-import org.linphone.core.Call;
-import org.linphone.core.Core;
-import org.linphone.core.CoreException;
-import org.linphone.core.CoreListenerStub;
-import org.linphone.core.GlobalState;
-import org.linphone.core.ProxyConfig;
-import org.linphone.core.RegistrationState;
-
-import java.lang.ref.WeakReference;
+import java.util.HashMap;
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
@@ -30,17 +25,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     String password = "101944957262#";
     String displayname = "17778115596";
     String domain = "cxc32.italkbb.com:10000";
-    //        String username="69802845066";
-//        String password="298613210000#";
-//        String displayname="15235661546";
-//        String domain="cxc32.italkbb.com:10000";
-//        String username="69801006278";
-//        String password="101204375147#";
-//        String displayname="15235661546";
-//        String domain="ose7.italkbb.com:10000";
+    //域名配置详细查看沙方洲发的"用户获取sip号码流程文档"
+    private String userAgentName = "softphone_android";
+    private String userAgentversion = "4.0.1";
+
+    HashMap<String,String> headMap=new HashMap<>();
+
     ActivityMainBinding binding;
-    private boolean isSpeakerEnabled = false, isMicMuted = false;
-    private boolean isPlayKey;
+    private boolean isSpeakerEnabled = false, isMicMuted = false, isPlayKey;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,7 +44,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             requestPermissions(strings, 0);
         }
         Objects.requireNonNull(getSupportActionBar()).hide();
-        LinphoneManager.initLoggingService(true, true, getString(R.string.app_name));
+//        LinphoneManager.initLoggingService(true, true, getString(R.string.app_name));
+        ////头部配置详细查看沙方洲发的"用户获取sip号码流程文档"
+        headMap.put("X-ITK-INFO", "con=wi;biz=fc;bid=12345");
     }
 
     @Override
@@ -60,87 +54,110 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch (v.getId()) {
             case R.id.linphone_tv1:
                 long time1 = System.currentTimeMillis();
-                SipUtils.getIns().init(new WeakReference<Context>(this), coreListener);
+                AJSipManager.getAccount().init(getApplicationContext(), userAgentName, userAgentversion, coreListener);
                 Log.d("linphone_初始化时间=", System.currentTimeMillis() - time1 + "");
                 break;
             case R.id.linphone_tv2:
-                SipUtils.getIns().call("69801006278", MainActivity.this);
+                String displayName="110";
+                AJSipManager.getCall().call("69801006278",displayName,headMap, getApplicationContext());
                 break;
             case R.id.linphone_tv3:
-                SipUtils.getIns().hangUp();
+                AJSipManager.getCall().hangUp();
                 break;
             case R.id.linphone_tv4:
-                SipUtils.getIns().answer( MainActivity.this);
+                AJSipManager.getCall().answer(getApplicationContext());
                 break;
             case R.id.linphone_tv5:
                 isMicMuted = !isMicMuted;
-                SipUtils.getIns().switchingMute(!isMicMuted);
+                AJSipManager.getCall().switchingMute(!isMicMuted);
                 binding.linphoneTv5.setText("静音" + String.valueOf(isMicMuted));
                 break;
             case R.id.linphone_tv6:
                 isSpeakerEnabled = !isSpeakerEnabled;
-                SipUtils.getIns().switchingSpeakers(isSpeakerEnabled);
+                AJSipManager.getCall().switchingSpeakers(isSpeakerEnabled);
                 binding.linphoneTv6.setText("免提" + String.valueOf(this.isSpeakerEnabled));
 
                 break;
             case R.id.linphone_tv7:
-                SipUtils.getIns().signOut();
+                AJSipManager.stop();
                 break;
             case R.id.linphone_tv8:
-                if (isPlayKey){
-                    SipUtils.getIns().stopDtmf();
-                }else {
-                    SipUtils.getIns().playDtmf(getContentResolver(), '6');
+                if (isPlayKey) {
+                    AJSipManager.getCall().stopDtmf();
+                } else {
+                    AJSipManager.getCall().playDtmf(getContentResolver(), '6');
                 }
-                isPlayKey=!isPlayKey;
+                isPlayKey = !isPlayKey;
                 break;
             case R.id.linphone_tv9:
-                SipUtils.getIns().sendDtmf('6');
+                AJSipManager.getCall().sendDtmf('6');
+                break;
+            case R.id.linphone_tv10:
+                binding.linphoneTv10.setText("10获取通话显示号码"+AJSipManager.getCall().getCurrentDisplayname());
+                break;
+            case R.id.linphone_tv11:
+                binding.linphoneTv11.setText("11获取通话号码"+AJSipManager.getCall().getCallUserName());
+                break;
+            case R.id.linphone_tv12:
+                binding.linphoneTv12.setText("12获取通话地址"+AJSipManager.getCall().getasStringUrl());
+                break;
+            case R.id.linphone_tv13:
+                binding.linphoneTv13.setText("13获取通话时长"+AJSipManager.getCall().getDirection());
+                break;
+            case R.id.linphone_tv14:
+                binding.linphoneTv14.setText("14获取登录账户代理"+AJSipManager.getAccount().getUserAgent());
+                break;
+            case R.id.linphone_tv15:
+                binding.linphoneTv15.setText("15获取登录账户域名"+AJSipManager.getAccount().getDomain(username));
+                break;
+            case R.id.linphone_tv16:
+                AJSipManager.getAccount().addListener(new AJCoreListenerStub(){
+                    @Override
+                    public void onCallStateChanged(AJCallState state, String message) {
+                        super.onCallStateChanged(state, message);
+                        Log.v("linphone2_打电话监听",message);
+                    }
+                });
+                break;
+            case R.id.linphone_tv17:
+                AJSipManager.getAccount().initLoggingService(true,true,getString(R.string.app_name));
                 break;
         }
     }
 
-    CoreListenerStub coreListener = new CoreListenerStub() {
+    AJCoreListenerStub coreListener = new AJCoreListenerStub() {
         @Override
-        public void onGlobalStateChanged(Core lc, GlobalState gstate, String message) {
-            super.onGlobalStateChanged(lc, gstate, message);
+        public void onGlobalStateChanged(AJGlobalState gstate, String message) {
+            super.onGlobalStateChanged(gstate, message);
             Log.d("linphone_初始化状态=", gstate.name() + "===" + message);
-            binding.setInitState("初始化状态"+message);
+            binding.setInitState("初始化状态" + message);
             /**
              * 初始化成功
              */
-            SipUtils.getIns().setOnLine(gstate == GlobalState.On);
-            if (gstate == GlobalState.On) {
-                long time1 = System.currentTimeMillis();
-                SipUtils.getIns().registerSip(username,
+            if (gstate == AJGlobalState.AJGlobal_On) {
+                AJSipManager.getAccount().registerSip(username,
                         password,
                         displayname,
-                        domain,new WeakReference<Context>(MainActivity.this));
-                Log.d("linphone_登录时间=", System.currentTimeMillis() - time1 + "");
+                        domain,headMap, getApplicationContext());
+
 
             }
         }
 
         @Override
-        public void onCallStateChanged(Core lc, Call call, Call.State cstate, String message) {
-            super.onCallStateChanged(lc, call, cstate, message);
-            Log.d("linphone_通话状态=", cstate.name() + "===" + message);
-            /**
-             * 通话状态
-             */
-            binding.setCallstate("通话状态"+message);
-
+        public void onRegistrationStateChanged(AJRegistrationState state, String message) {
+            super.onRegistrationStateChanged(state, message);
+            Log.d("linphone_注册状态=", state.toString() + "===" + message);
+            //注册成功
+            binding.setRegisterState("注册状态" + message);
         }
 
         @Override
-        public void onRegistrationStateChanged(Core lc, ProxyConfig cfg, RegistrationState cstate, String message) {
-            super.onRegistrationStateChanged(lc, cfg, cstate, message);
-            Log.d("linphone_注册状态=", cstate.toString() + "===" + message);
-            /**
-             * 注册成功
-             */
-            SipUtils.getIns().setRegister(cstate == RegistrationState.Ok);
-            binding.setRegisterState("注册状态"+message);
+        public void onCallStateChanged(AJCallState state, String message) {
+            super.onCallStateChanged(state, message);
+            Log.d("linphone_通话状态=", state.name() + "===" + message);
+//            //通话状态
+            binding.setCallstate("通话状态" + message);
         }
     };
 }
